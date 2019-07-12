38d40ab1a82dc9c2d8f7f0d955b21628265ff691
Allen <kaodondon7@gmail.com>
2019-07-12 17:48:56 +0800

    add log and test code

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package canopus
 import (
 	"bytes"
 	"crypto/rand"
+	"golang.org/x/exp/errors/fmt"
 	"log"
 	"net"
 	"strconv"
@@ -419,8 +420,10 @@ func (s *DefaultCoapServer) handleIncomingDTLSData(conn ServerConnection, ctx *S
 						panic(err.Error())
 					}
 					s.sessions[addr.String()] = ssn
-					s.createdSession <- ssn
+				} else {
+					log.Println(fmt.Sprint("session already exist: %v", addr.String()))
 				}
+				s.createdSession <- ssn
 				s.Unlock()
 				ssn.(*DTLSServerSession).rcvd <- msgBuf
 			} else {
